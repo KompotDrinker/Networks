1fe8c1ede865151e50786511e137783dfe07613a
KompotDrinker <lyayar2002@gmail.com>
2025-01-12 21:58:48 +0200

    Added requirements files

---
+++ __temp_pipreqsnb_folder/_pipreqsnb_temp_file.py
@@ -0,0 +1,32 @@
+import os
+import rasterio
+from rasterio.plot import reshape_as_image
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import kornia as K
+from kornia_moons.viz import draw_LAF_matches
+import os
+import keras
+from keras import ops
+import numpy as np
+import tensorflow as tf
+from keras import layers
+from datasets import load_dataset
+from collections import Counter
+import os
+import rasterio
+from rasterio.plot import reshape_as_image
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import kornia as K
+from kornia_moons.viz import draw_LAF_matches
+import os
+import keras
+from keras import ops
+import numpy as np
+import tensorflow as tf
+from keras import layers
+from datasets import load_dataset
+from collections import Counter
